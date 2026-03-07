af81c361217b6662e0482c0ba2f76ff8fe874415
Teo Benavides <730wipu@gmail.com>
2026-03-06 21:21:42 -0300

    transform empty full name to none

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,11 +1,19 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
 from app.schemas.base import CamelModel
+
 
 class UserCreate(CamelModel):
     email: EmailStr
     password: str
     username: str
     full_name: str | None
+
+    @field_validator("full_name", mode="before")
+    @classmethod
+    def empty_str_to_none(cls, v: str | None) -> str | None:
+        if v == "":
+            return None
+        return v
 
 class UserLogin(CamelModel):
     email_or_username: str
